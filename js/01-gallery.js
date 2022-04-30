719434147356f01a7ca883136dd5a44c2f6a4d32
Vlad Koblay <75365371+VKChabra@users.noreply.github.com>
2022-04-30 16:09:56 +0300

    one half done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//preview, original, description
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const makeGalleryItems = element => {
+    const { preview, original, description } = element;
+
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+};
+
+const galleryItemsList = galleryItems.map(makeGalleryItems).join('')
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsList)
+
+const instance = basicLightbox.create(`<img class='gallery__modal-img' src=''>`,
+    {
+        onShow: () => document.addEventListener('keydown', escButtonCloseInstance),
+    },
+);
+
+const escButtonCloseInstance = e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+}
+
+const openGalleryModal = e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    instance.element().querySelector('.gallery__modal-img').src = e.target.dataset.source;
+    instance.show();
+};
+
+gallery.addEventListener("click", openGalleryModal);
