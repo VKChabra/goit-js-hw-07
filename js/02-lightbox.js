8647ce88cf34cbc6e03ee808f84493379fbb56ce
Vlad Koblay <75365371+VKChabra@users.noreply.github.com>
2022-04-30 16:47:19 +0300

    patch 1.0

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//preview, original, description
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const makeGalleryItems = element => {
+    const { preview, original, description } = element;
+
+    return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>`;
+};
+
+const galleryItemsList = galleryItems.map(makeGalleryItems).join('')
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsList)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    close: true,
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom',
+    showCounter: true,
+    doubleTapZoom: 2,
+});
